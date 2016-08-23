a828d052692af572be38a0e5e59f75b43011aa3a
Kranthi Kumar <kuppula@processmap.com>
2016-08-24 03:22:47 +0530

    fixing width for iframe if screenwidth<=768
    This will fix any horizontal scrolling, some fancybox issues

--- src/components/Iframe.js
+++ src/components/Iframe.js
@@ -8,13 +8,14 @@ class Iframe extends React.Component {
   // Render method.
   render() {
     const { shUi, globalSettings, url } = this.props;
+    const iframewidth = screen.width > 768 ? '100%' : shUi.browserInfo.width - 50;
 
     const styles = {
       iframe: {
         position: 'absolute',
         top: 0,
         left: 0,
-        width: shUi.isNavigatorPinned && globalSettings.selectedMenuItem.IsNavigatorDisplay ? 'calc(100% - 274px)' : '100%',
+        width: shUi.isNavigatorPinned && globalSettings.selectedMenuItem.IsNavigatorDisplay ? 'calc(100% - 274px)' : iframewidth,
         height: shUi.browserInfo.height - 100,
         border: 0,
         transition: 'all .5s ease',
@@ -22,7 +23,7 @@ class Iframe extends React.Component {
     };
 
     return (
-      <iframe id="iframeComponent" src={url} scrolling="yes" className="iframeWrapper home" style={styles.iframe} />
+        <iframe id="iframeComponent" src={url} scrolling={ screen.width > 768 ? 'yes' : 'no' } className="iframeWrapper home" style={styles.iframe} />
     );
   }
 }
